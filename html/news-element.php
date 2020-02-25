cb470aa7a84304dd40300f2c20a6422f8f2720a7
Pavel Ryumin <pryumin@nextype.ru>
2020-02-25 18:56:55 +0500

    Solutions, News и News-element

---
+++ html/news-element.php
@@ -0,0 +1,40 @@
+<?php
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
+$APPLICATION->SetTitle("Главная страница сайта");
+?>
+
+    <div class="content-wrapper">
+        <div class="news-element-page">
+            <div class="container">
+                <div class="content">
+                    <div class="row no-gutters actions">
+                        <a href="#" class="link-back">
+                            <i class="icon icon-arrow-left-line"></i>
+                            <span class='link-text font-paragraph-base font-bold'>Back</span>
+                        </a>
+                        <div class="date-wrap">
+                            <i class="icon icon-calendar-event-line"></i>
+                            <span class="date font-paragraph-base font-bold">22/11/2019</span>
+                        </div>
+                    </div>
+                    <div class="text font-paragraph-lead">
+                        See how a collaboration between DSM, Natuurmonumenten, Wagening UR and Compakboard has developed a sustainable building material based on reeds cleared to maintain the unique habitat of the De Wieden National Park.
+                    </div>
+                    <div class="video-container">
+                         <video src="https://www.youtube.com/watch?v=WTWyosdkx44"></video>
+                    </div>
+                   
+                </div>
+            </div>
+        </div>
+    </div>
+    
+
+
+    <script>
+          wow = new WOW();
+          wow.init();
+    </script>
+<?
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
+?>
---
+++ html/news.php
@@ -0,0 +1,194 @@
+<?php
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
+$APPLICATION->SetTitle("Главная страница сайта");
+?>
+
+    <div class="content-wrapper">
+        <div class="news-page">
+            <div class="container">
+                <div class="items row">
+                    <div class="item-wrap col-md-4 col-sm-6 col-12">
+                        <div class="item">
+                            <a href="#" class="img" style="background-image: url('https://s3-alpha-sig.figma.com/img/dc54/9389/f0888ed759c94ebd8d9167e6d6fcd199?Expires=1583712000&Signature=ICAtO3Zs24XFICSMAn5pkvSc5B13fWnzelBgDnH7usOIqmcF8szT~Gk~jrdOaZrQTbdASaar7aZqHHqzGqRmVARsjXoqEQ1mpqcy8s1pyfJ1Xsac4b7Ymayajg4phuw71ahnvkU6VgiBhXTWiPtptSG~77mNYGk7ShFCRBm6orduD~wxzCy3PaW3HxliRMEobioG23FZeHIJ3RUOihntuXLfmTIhAjqWQzxJ2SIdGa1xsRitL4UVTq0aIanvwID6brnJk875fQXTOJgxOp6SMcKOrxNnhBcH5PrKY~A8q2OxUQYCMglSOdOOQiBjm1RYdwDkjq0k2u5cRDFPtdqxAQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA')"></a>
+                            <div class="news-item-information">
+                                <a href="#" class="h3 title">DSM Global Climate Change</a>
+                                <div class="row no-gutters info-elements">
+                                    <div class="date-wrap news-info-element">
+                                        <i class="icon icon-calendar-event-line"></i>
+                                        <span class="date font-paragraph-base font-bold">22/11/2019</span>
+                                    </div>
+                                    <a href="#" class="read-more news-info-element">
+                                        <i class="icon icon-add-line"></i>
+                                        <span>Show More</span>
+                                    </a>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item-wrap col-md-4 col-sm-6 col-12">
+                        <div class="item">
+                            <a href="#" class="img" style="background-image: url('https://s3-alpha-sig.figma.com/img/dc54/9389/f0888ed759c94ebd8d9167e6d6fcd199?Expires=1583712000&Signature=ICAtO3Zs24XFICSMAn5pkvSc5B13fWnzelBgDnH7usOIqmcF8szT~Gk~jrdOaZrQTbdASaar7aZqHHqzGqRmVARsjXoqEQ1mpqcy8s1pyfJ1Xsac4b7Ymayajg4phuw71ahnvkU6VgiBhXTWiPtptSG~77mNYGk7ShFCRBm6orduD~wxzCy3PaW3HxliRMEobioG23FZeHIJ3RUOihntuXLfmTIhAjqWQzxJ2SIdGa1xsRitL4UVTq0aIanvwID6brnJk875fQXTOJgxOp6SMcKOrxNnhBcH5PrKY~A8q2OxUQYCMglSOdOOQiBjm1RYdwDkjq0k2u5cRDFPtdqxAQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA')"></a>
+                            <div class="news-item-information">
+                                <a href="#" class="h3 title">DSM Global Climate Change</a>
+                                <div class="row no-gutters info-elements">
+                                    <div class="date-wrap news-info-element">
+                                        <i class="icon icon-calendar-event-line"></i>
+                                        <span class="date font-paragraph-base font-bold">22/11/2019</span>
+                                    </div>
+                                    <a href="#" class="read-more news-info-element">
+                                        <i class="icon icon-add-line"></i>
+                                        <span>Show More</span>
+                                    </a>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item-wrap col-md-4 col-sm-6 col-12">
+                        <div class="item">
+                            <a href="#" class="img" style="background-image: url('https://s3-alpha-sig.figma.com/img/dc54/9389/f0888ed759c94ebd8d9167e6d6fcd199?Expires=1583712000&Signature=ICAtO3Zs24XFICSMAn5pkvSc5B13fWnzelBgDnH7usOIqmcF8szT~Gk~jrdOaZrQTbdASaar7aZqHHqzGqRmVARsjXoqEQ1mpqcy8s1pyfJ1Xsac4b7Ymayajg4phuw71ahnvkU6VgiBhXTWiPtptSG~77mNYGk7ShFCRBm6orduD~wxzCy3PaW3HxliRMEobioG23FZeHIJ3RUOihntuXLfmTIhAjqWQzxJ2SIdGa1xsRitL4UVTq0aIanvwID6brnJk875fQXTOJgxOp6SMcKOrxNnhBcH5PrKY~A8q2OxUQYCMglSOdOOQiBjm1RYdwDkjq0k2u5cRDFPtdqxAQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA')"></a>
+                            <div class="news-item-information">
+                                <a href="#" class="h3 title">DSM Global Climate Change</a>
+                                <div class="row no-gutters info-elements">
+                                    <div class="date-wrap news-info-element">
+                                        <i class="icon icon-calendar-event-line"></i>
+                                        <span class="date font-paragraph-base font-bold">22/11/2019</span>
+                                    </div>
+                                    <a href="#" class="read-more news-info-element">
+                                        <i class="icon icon-add-line"></i>
+                                        <span>Show More</span>
+                                    </a>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item-wrap col-md-4 col-sm-6 col-12">
+                        <div class="item">
+                            <a href="#" class="img" style="background-image: url('https://s3-alpha-sig.figma.com/img/dc54/9389/f0888ed759c94ebd8d9167e6d6fcd199?Expires=1583712000&Signature=ICAtO3Zs24XFICSMAn5pkvSc5B13fWnzelBgDnH7usOIqmcF8szT~Gk~jrdOaZrQTbdASaar7aZqHHqzGqRmVARsjXoqEQ1mpqcy8s1pyfJ1Xsac4b7Ymayajg4phuw71ahnvkU6VgiBhXTWiPtptSG~77mNYGk7ShFCRBm6orduD~wxzCy3PaW3HxliRMEobioG23FZeHIJ3RUOihntuXLfmTIhAjqWQzxJ2SIdGa1xsRitL4UVTq0aIanvwID6brnJk875fQXTOJgxOp6SMcKOrxNnhBcH5PrKY~A8q2OxUQYCMglSOdOOQiBjm1RYdwDkjq0k2u5cRDFPtdqxAQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA')"></a>
+                            <div class="news-item-information">
+                                <a href="#" class="h3 title">DSM Global Climate Change</a>
+                                <div class="row no-gutters info-elements">
+                                    <div class="date-wrap news-info-element">
+                                        <i class="icon icon-calendar-event-line"></i>
+                                        <span class="date font-paragraph-base font-bold">22/11/2019</span>
+                                    </div>
+                                    <a href="#" class="read-more news-info-element">
+                                        <i class="icon icon-add-line"></i>
+                                        <span>Show More</span>
+                                    </a>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item-wrap col-md-4 col-sm-6 col-12">
+                        <div class="item">
+                            <a href="#" class="img" style="background-image: url('https://s3-alpha-sig.figma.com/img/dc54/9389/f0888ed759c94ebd8d9167e6d6fcd199?Expires=1583712000&Signature=ICAtO3Zs24XFICSMAn5pkvSc5B13fWnzelBgDnH7usOIqmcF8szT~Gk~jrdOaZrQTbdASaar7aZqHHqzGqRmVARsjXoqEQ1mpqcy8s1pyfJ1Xsac4b7Ymayajg4phuw71ahnvkU6VgiBhXTWiPtptSG~77mNYGk7ShFCRBm6orduD~wxzCy3PaW3HxliRMEobioG23FZeHIJ3RUOihntuXLfmTIhAjqWQzxJ2SIdGa1xsRitL4UVTq0aIanvwID6brnJk875fQXTOJgxOp6SMcKOrxNnhBcH5PrKY~A8q2OxUQYCMglSOdOOQiBjm1RYdwDkjq0k2u5cRDFPtdqxAQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA')"></a>
+                            <div class="news-item-information">
+                                <a href="#" class="h3 title">DSM Global Climate Change</a>
+                                <div class="row no-gutters info-elements">
+                                    <div class="date-wrap news-info-element">
+                                        <i class="icon icon-calendar-event-line"></i>
+                                        <span class="date font-paragraph-base font-bold">22/11/2019</span>
+                                    </div>
+                                    <a href="#" class="read-more news-info-element">
+                                        <i class="icon icon-add-line"></i>
+                                        <span>Show More</span>
+                                    </a>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item-wrap col-md-4 col-sm-6 col-12">
+                        <div class="item">
+                            <a href="#" class="img" style="background-image: url('https://s3-alpha-sig.figma.com/img/dc54/9389/f0888ed759c94ebd8d9167e6d6fcd199?Expires=1583712000&Signature=ICAtO3Zs24XFICSMAn5pkvSc5B13fWnzelBgDnH7usOIqmcF8szT~Gk~jrdOaZrQTbdASaar7aZqHHqzGqRmVARsjXoqEQ1mpqcy8s1pyfJ1Xsac4b7Ymayajg4phuw71ahnvkU6VgiBhXTWiPtptSG~77mNYGk7ShFCRBm6orduD~wxzCy3PaW3HxliRMEobioG23FZeHIJ3RUOihntuXLfmTIhAjqWQzxJ2SIdGa1xsRitL4UVTq0aIanvwID6brnJk875fQXTOJgxOp6SMcKOrxNnhBcH5PrKY~A8q2OxUQYCMglSOdOOQiBjm1RYdwDkjq0k2u5cRDFPtdqxAQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA')"></a>
+                            <div class="news-item-information">
+                                <a href="#" class="h3 title">DSM Global Climate Change</a>
+                                <div class="row no-gutters info-elements">
+                                    <div class="date-wrap news-info-element">
+                                        <i class="icon icon-calendar-event-line"></i>
+                                        <span class="date font-paragraph-base font-bold">22/11/2019</span>
+                                    </div>
+                                    <a href="#" class="read-more news-info-element">
+                                        <i class="icon icon-add-line"></i>
+                                        <span>Show More</span>
+                                    </a>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item-wrap col-md-4 col-sm-6 col-12">
+                        <div class="item">
+                            <a href="#" class="img" style="background-image: url('https://s3-alpha-sig.figma.com/img/dc54/9389/f0888ed759c94ebd8d9167e6d6fcd199?Expires=1583712000&Signature=ICAtO3Zs24XFICSMAn5pkvSc5B13fWnzelBgDnH7usOIqmcF8szT~Gk~jrdOaZrQTbdASaar7aZqHHqzGqRmVARsjXoqEQ1mpqcy8s1pyfJ1Xsac4b7Ymayajg4phuw71ahnvkU6VgiBhXTWiPtptSG~77mNYGk7ShFCRBm6orduD~wxzCy3PaW3HxliRMEobioG23FZeHIJ3RUOihntuXLfmTIhAjqWQzxJ2SIdGa1xsRitL4UVTq0aIanvwID6brnJk875fQXTOJgxOp6SMcKOrxNnhBcH5PrKY~A8q2OxUQYCMglSOdOOQiBjm1RYdwDkjq0k2u5cRDFPtdqxAQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA')"></a>
+                            <div class="news-item-information">
+                                <a href="#" class="h3 title">DSM Global Climate Change</a>
+                                <div class="row no-gutters info-elements">
+                                    <div class="date-wrap news-info-element">
+                                        <i class="icon icon-calendar-event-line"></i>
+                                        <span class="date font-paragraph-base font-bold">22/11/2019</span>
+                                    </div>
+                                    <a href="#" class="read-more news-info-element">
+                                        <i class="icon icon-add-line"></i>
+                                        <span>Show More</span>
+                                    </a>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item-wrap col-md-4 col-sm-6 col-12">
+                        <div class="item">
+                            <a href="#" class="img" style="background-image: url('https://s3-alpha-sig.figma.com/img/dc54/9389/f0888ed759c94ebd8d9167e6d6fcd199?Expires=1583712000&Signature=ICAtO3Zs24XFICSMAn5pkvSc5B13fWnzelBgDnH7usOIqmcF8szT~Gk~jrdOaZrQTbdASaar7aZqHHqzGqRmVARsjXoqEQ1mpqcy8s1pyfJ1Xsac4b7Ymayajg4phuw71ahnvkU6VgiBhXTWiPtptSG~77mNYGk7ShFCRBm6orduD~wxzCy3PaW3HxliRMEobioG23FZeHIJ3RUOihntuXLfmTIhAjqWQzxJ2SIdGa1xsRitL4UVTq0aIanvwID6brnJk875fQXTOJgxOp6SMcKOrxNnhBcH5PrKY~A8q2OxUQYCMglSOdOOQiBjm1RYdwDkjq0k2u5cRDFPtdqxAQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA')"></a>
+                            <div class="news-item-information">
+                                <a href="#" class="h3 title">DSM Global Climate Change</a>
+                                <div class="row no-gutters info-elements">
+                                    <div class="date-wrap news-info-element">
+                                        <i class="icon icon-calendar-event-line"></i>
+                                        <span class="date font-paragraph-base font-bold">22/11/2019</span>
+                                    </div>
+                                    <a href="#" class="read-more news-info-element">
+                                        <i class="icon icon-add-line"></i>
+                                        <span>Show More</span>
+                                    </a>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item-wrap col-md-4 col-sm-6 col-12">
+                        <div class="item">
+                            <a href="#" class="img" style="background-image: url('https://s3-alpha-sig.figma.com/img/dc54/9389/f0888ed759c94ebd8d9167e6d6fcd199?Expires=1583712000&Signature=ICAtO3Zs24XFICSMAn5pkvSc5B13fWnzelBgDnH7usOIqmcF8szT~Gk~jrdOaZrQTbdASaar7aZqHHqzGqRmVARsjXoqEQ1mpqcy8s1pyfJ1Xsac4b7Ymayajg4phuw71ahnvkU6VgiBhXTWiPtptSG~77mNYGk7ShFCRBm6orduD~wxzCy3PaW3HxliRMEobioG23FZeHIJ3RUOihntuXLfmTIhAjqWQzxJ2SIdGa1xsRitL4UVTq0aIanvwID6brnJk875fQXTOJgxOp6SMcKOrxNnhBcH5PrKY~A8q2OxUQYCMglSOdOOQiBjm1RYdwDkjq0k2u5cRDFPtdqxAQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA')"></a>
+                            <div class="news-item-information">
+                                <a href="#" class="h3 title">DSM Global Climate Change</a>
+                                <div class="row no-gutters info-elements">
+                                    <div class="date-wrap news-info-element">
+                                        <i class="icon icon-calendar-event-line"></i>
+                                        <span class="date font-paragraph-base font-bold">22/11/2019</span>
+                                    </div>
+                                    <a href="#" class="read-more news-info-element">
+                                        <i class="icon icon-add-line"></i>
+                                        <span>Show More</span>
+                                    </a>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+
+
+    <script>
+          wow = new WOW();
+          wow.init();
+    </script>
+<?
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
+?>
