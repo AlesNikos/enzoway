731411295cdb2a7c1e30e1c111ba1ffeff1a2a2f
Pavel Ryumin <pryumin@nextype.ru>
2020-02-25 12:20:58 +0500

    Страница science

---
+++ html/science.php
@@ -0,0 +1,49 @@
+<?php
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
+$APPLICATION->SetTitle("Главная страница сайта");
+?>
+
+    <div class="content-wrapper">
+        <div class="science-page">
+            <div class="container">
+                <div class="items">
+                    <div class="item">
+                        <div class="h2">European Stressless Flour</div>
+                        <div class="description">
+                            This material is a logical continuation of the series of articles about the features of the European flour mill, published in the September and October issues of the magazine: "Comparison of the principles of operation of Russian and European flour mills" and "Secrets of European flour mills".
+                        </div>
+                        <div class="row no-gutters info-block">
+                            <div class="col-auto info-block-item views">
+                                <div class="row no-gutters font-paragraph-base font-bold">
+                                    <i class="icon icon-eye-line"></i>
+                                    <span>345</span>
+                                </div>
+                            </div>
+                            <div class="col-auto info-block-item read-more">
+                                 <div class="row no-gutters font-paragraph-base font-bold">
+                                     <a href="#" class="link">
+                                        <i class="icon icon-add-line"></i>
+                                        <span>Read More</span>
+                                     </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item"></div>
+                    <div class="item"></div>
+                    <div class="item"></div>
+                    <div class="item"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+
+
+    <script>
+          wow = new WOW();
+          wow.init();
+    </script>
+<?
+require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
+?>
