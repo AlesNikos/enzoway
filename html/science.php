58f2afdb10e6bd29acbd6e8a17b67e0de599b0ea
Pavel Ryumin <pryumin@nextype.ru>
2020-02-27 10:55:16 +0500

    +

--- html/science.php
+++ html/science.php
@@ -47,6 +47,17 @@ $APPLICATION->SetTitle("Главная страница сайта");
                     <div class="item"></div>
                     <div class="item"></div>
                 </div>
+                <div class="pagination-nav">
+                    <nav>
+                        <ul class="pagination">
+                            <li class="page-item active"><a class="page-link" href="#">1</a></li>
+                            <li class="page-item"><a class="page-link" href="#">2</a></li>
+                            <li class="page-item"><a class="page-link" href="#">3</a></li>
+                            <li class="page-item"><a class="page-link" href="#">...</a></li>
+                            <li class="page-item"><a class="page-link" href="#">70</a></li>
+                        </ul>
+                    </nav>
+                </div>
             </div>
         </div>
     </div>
